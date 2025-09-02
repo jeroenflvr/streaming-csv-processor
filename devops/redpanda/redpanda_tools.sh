ea3294fe2a0321f94dd4f7320307af1440cd0d3f
jeroenflvr <jeroen@flexworks.eu>
2025-09-02 20:54:00 +0200

    fix: producer config

--- devops/redpanda/redpanda_tools.sh
+++ devops/redpanda/redpanda_tools.sh
@@ -3,28 +3,28 @@
 REDPANDA_PATH="/Users/jeroen/projects/streaming-csv-processor/devops/redpanda"
 
 start_redpanda() {
-    echo "starting redpanda ..."
-    cwd=$(pwd)
-    cd $REDPANDA_PATH
-    docker compose up -d
-    cd $cwd
- 
-}
- 
-stop_redpanda() {
-    echo "stopping redpanda ..."
-    cwd=$(pwd)
-    cd $REDPANDA_PATH
-    docker compose down
-    cd $cwd
-}
- 
+  echo "starting redpanda ..."
+  cwd=$(pwd)
+  cd $REDPANDA_PATH
+  docker compose up -d
+  cd $cwd
 
-create_topics(){
-    rpk topic create local-stream-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
-    rpk topic create local-input-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
-    rpk topic create local-update-topic --brokers localhost:9093 -c cleanup.policy=compact -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
-    rpk topic create local-state-topic --brokers localhost:9093 -c cleanup.policy=compact -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
-    rpk topic create local-updates-only-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
 }
- 
+
+stop_redpanda() {
+  echo "stopping redpanda ..."
+  cwd=$(pwd)
+  cd $REDPANDA_PATH
+  docker compose down
+  cd $cwd
+}
+
+create_topics() {
+  rpk topic create local-stream-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+  rpk topic create local-input-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+  rpk topic create local-update-topic --brokers localhost:9093 -c cleanup.policy=compact -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+  rpk topic create local-state-topic --brokers localhost:9093 -c cleanup.policy=compact -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+  rpk topic create local-updates-only-topic --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+  rpk topic create failed-messages --brokers localhost:9093 -X tls.enabled=true --tls-truststore=/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt
+}
+
--- src/main/java/jeroenflvr/csvprocessor/kafka/FailedMessageProducer.java
+++ src/main/java/jeroenflvr/csvprocessor/kafka/FailedMessageProducer.java
@@ -17,8 +17,15 @@ public class FailedMessageProducer {
     public FailedMessageProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv().getOrDefault("BOOTSTRAP_SERVERS", "localhost:9093"));
+        props.put("security.protocol", System.getenv().getOrDefault("KAFKA_SECURITY_PROTOCOL", "SSL"));
+        props.put("ssl.ca.location", System.getenv().getOrDefault("KAFKA_SSL_CA_LOCATION", "/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/ca.crt"));
+        props.put("client.id", System.getenv().getOrDefault("KAFKA_CLIENT_ID", "scala-producer"));
+        props.put("enable.idempotence", System.getenv().getOrDefault("KAFKA_ENABLE_IDEMPOTENCE", "true"));
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put("ssl.endpoint.identification.algorithm", "");
+        props.put("ssl.truststore.location", System.getenv().getOrDefault("KAFKA_SSL_TRUSTSTORE_LOCATION", "/Users/jeroen/projects/streaming-csv-processor/devops/redpanda/certs/truststore.jks"));
+        props.put("ssl.truststore.password", System.getenv().getOrDefault("KAFKA_SSL_TRUSTSTORE_PASSWORD", "changeit"));
         this.producer = new KafkaProducer<>(props);
     }
 
